7a10a53a042b3ed97f927d8474a49233d07cd666
Neo <431738916@qq.com>
2025-05-08 00:28:31 +0800

    docs(Python变量): 添加变量全解析文档及示例代码
    - 新增 Python 变量全解析文档，系统介绍变量的概念、定义方法、作用和注意事项
    - 添加示例代码文件，演示变量的不同赋值方式和使用场景
    - 文档和代码覆盖了变量的常见用法，便于初学者理解和掌握 Python 变量的使用

---
+++ Python变量/example.py
@@ -0,0 +1,86 @@
+# 方式1
+# 变量名 = 值
+# a = 1
+# print(a)
+
+# a = 2
+# b = 3
+# print(a, b)
+
+# 方式2
+# 变量名1, 变量名2 = 值1, 值2
+
+# a, b = 1, 2
+# print(a, b)
+
+# 方式3
+# a = 6
+# b = 6
+# a = b = 3
+# print(a, b)
+
+# 6 + 1; 6 + 2; 6 + 3; 6 + 4; 6 + 5;
+# print(6 + 1)
+# print(6 + 2)
+# print(6 + 3)
+# print(6 + 4)
+# print(6 + 5)
+
+# num = 8
+# print(num + 1)
+# print(num + 2)
+# print(num + 3)
+# print(num + 4)
+# print(num + 5)
+
+# 节省空间（磁盘，内存）
+# 打印十遍 “社会我NEO哥，人狠话不多 1”
+# print("社会我NEO哥，人狠话不多 1")
+# print("社会我NEO哥，人狠话不多 2")
+# print("社会我NEO哥，人狠话不多 3")
+# print("社会我NEO哥，人狠话不多 4")
+# print("社会我NEO哥，人狠话不多 5")
+# print("社会我NEO哥，人狠话不多 6")
+# print("社会我NEO哥，人狠话不多 7")
+# print("社会我NEO哥，人狠话不多 8")
+# print("社会我NEO哥，人狠话不多 9")
+# print("社会我NEO哥，人狠话不多 10")
+
+# name = "社会我NEO哥，人狠话不多"
+
+# print(name + "1")
+# print(name + "2")
+# print(name + "3")
+# print(name + "4")
+# print(name + "5")
+# print(name + "6")
+# print(name + "7")
+# print(name + "8")
+# print(name + "9")
+# print(name + "10")
+
+# age = 10
+# age = 11
+# print(age)
+
+# 字母数字下划线
+# name = 'sz'
+# 4_name2_ = "sz2"
+
+# 驼峰标识
+# phoneNum = "18819223212"
+
+# 非关键字
+# class = 10
+
+# 查看系统关键字
+# import keyword
+
+# print(keyword.kwlist)
+
+# name = 'sz'
+# nAme = 'sz2'
+# print(name, nAme)
+
+# 变量名使用之前一定要赋值
+# print(a)
