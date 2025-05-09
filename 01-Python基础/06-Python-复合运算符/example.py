641f82bb8825407e5a4360247695d3ddb2d77594
Neo <431738916@qq.com>
2025-05-09 13:47:55 +0800

    docs(01-Python基础/06-Python-复合运算符): 添加复合赋值运算符示例及文档
    - 新增 example.py 文件，演示各种复合赋值运算符的使用方法
    - 添加 Python赋值运算符全解.md 文档，详细解释了复合赋值运算符的用法和特点
    - 覆盖了 +=, *=, /=, %=, //=, **= 等运算符的示例和说明

---
+++ 01-Python基础/06-Python-复合运算符/example.py
@@ -0,0 +1,37 @@
+# num = 10
+# result = num + 5
+# num = result
+# print(num)
+
+# num = 10
+# num = num + 5
+# print(num)
+
+# num = 10
+# num += 5
+# print(num)
+
+# *=
+# num = 10
+# num *= 5
+# print(num)
+
+# /=
+# num = 10
+# num /= 5
+# print(num)
+
+# %=
+# num = 10
+# num %= 5
+# print(num)
+
+# //=
+# num = 10
+# num //= 5
+# print(num)
+
+# **=
+num = 10
+num **= 5
+print(num)
