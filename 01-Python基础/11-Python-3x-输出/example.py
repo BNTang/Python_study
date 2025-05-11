45cf5693df67f8d3c8431bcd8fe58ffbdcdaa373
Neo <431738916@qq.com>
2025-05-11 13:33:33 +0800

    docs(01-Python基础/11-Python-3x-输出): 新增print()函数详解及示例代码

---
+++ 01-Python基础/11-Python-3x-输出/example.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# 输出一个值
+# print(123)
+
+# 输出一个变量
+# num = 55
+# print(num)
+
+# 输出多个变量
+# num = 10
+# name = '小明'
+# print(num, name)
+
+# 格式化输出
+# name = '小明'
+# age = 18
+# 我的名字是xxx,今年xxx岁
+# print('我的名字是%s,今年%s岁' % (name, age))
+# print('我的名字是{0},今年{1}岁'.format(name, age))
+
+# 输出到文件中
+# f = open('test.txt', 'w', encoding='utf-8')
+# print("xxxxxxxxx", file=f)
+
+# import sys
+# print("xxxxxxxxx", file=sys.stdout)
+
+# 输出不自动换行
+# print("123", end='')
+# print("123", end='')
+
+# 输出的各个数据,使用分隔符分割
+# print("123", "456", "789", sep=',')
+
+# flush 参数的说明
+# print("请输入账号", end="", flush=True)
+
+# 休眠5s
+# from time import sleep
+# sleep(5)
