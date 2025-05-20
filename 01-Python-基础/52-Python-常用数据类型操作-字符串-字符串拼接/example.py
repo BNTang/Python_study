b500dbbfd0d6a8d68d53b0f2bfde5a994297da8e
Neo <431738916@qq.com>
2025-05-20 14:33:27 +0800

    docs: 添加Python字符串拼接教程和示例代码
    新增了Python字符串拼接的详细教程文档及配套示例代码，包括以下内容：
    
    - 使用+运算符进行字符串拼接
    - 字符串字面量直接连接
    - %运算符格式化字符串
    - 字符串重复（字符串乘法）操作
    - f-字符串格式化（Python 3.6+）
    - join()方法等高级用法
    
    文档包含了每种方法的详细说明和代码示例，帮助开发者选择最适合的字符串拼接方式。

---
+++ 01-Python-基础/52-Python-常用数据类型操作-字符串-字符串拼接/example.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-20 14:18:34
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 52-Python-常用数据类型操作-字符串-字符串拼接
+
+# +
+# 字符串拼接
+# 1. 使用 + 运算符
+# str1 = "Hello"
+# str2 = "World"
+# str3 = str1 + " " + str2
+# print(str3)  # 输出: Hello World
+
+# str1 + str2
+
+# 字符串拼接
+
+# 方式1
+
+# 方式2 
+
+# 方式3 。
+
+# 方式4 。
+
+# 例如:
+
+# str1str2
+
+# 例如:
+
+# "wangzha" + "shunzi'
+
+# wangzhashunzi
+
+# 直接把两个字符串放一块
+
+# "wangzha""shunzi'
+
+# wangzhashunzi
+
+# “%(a+b)
+
+# xxx%sxxx
+
+# 字符串乘法
+
+# “abc"* 3
+
+# "abcabcabc"
+
+# 空格拼接，但是要保证内容在一行
+# result = "Hello"        "World"
+# print(result)  # 输出: HelloWorld
+
+# 字符串模板
+# result = "Welcome to %s" % "Python"
+# print(result)  # 输出: Welcome to Python
+
+# 字符串模板填写多个
+# result = "Welcome to %s, you are %d years old." % ("Python", 30)
+# print(result)  # 输出: Welcome to Python, you are 30 years old.
+
+# 打印多少遍内容
+# result = "Hello" * 3
+# print(result)  # 输出: HelloHelloHello
+
+# \t
+# result = "Hello\tWorld"
+# print(result)  # 输出: Hello    World
