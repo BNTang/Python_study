e0434908656a005596da897cd5334bfd1a8e7dbf
Neo <431738916@qq.com>
2025-05-20 23:43:25 +0800

    docs: 优化 Python 字符串函数操作相关文档结构
    重新组织了 Python 字符串相关的教程文档，将内容拆分为更细粒度的章节：
    
    - 删除原有的字符串拼接教程文档
    - 新增字符串函数操作查找计算示例
    - 调整了示例代码的注释和描述信息
    
    这次重构使教程结构更清晰，便于学习者逐步掌握 Python 字符串的各项操作。

---
+++ 01-Python-基础/55-Python-常用数据类型操作-字符串-函数操作-查找计算/example.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-20 14:18:34
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 55-Python-常用数据类型操作-字符串-函数操作-查找计算
+
+# len
+# name = "Python"
+# num = len(name)
+# print("字符串长度为：", num)
+
+name = "我是 Python"
+num = len(name)
+print("字符串长度为：", num)
