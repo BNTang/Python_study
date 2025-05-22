969e8b616632fce5ec580c5386d716a9f70d236c
Neo <431738916@qq.com>
2025-05-22 13:30:58 +0800

    docs: 新增 Python 列表修改操作教程文档和示例代码
    添加了详细的 Python 列表修改操作教程文档，包含以下内容：
    - 基本语法介绍和使用示例
    - 常见错误和注意事项说明
    - 其他常用列表修改方法概述
    
    同时提供了对应的示例代码，展示了列表元素修改的基本操作和错误处理示例。
    
    教程内容针对 Python 初学者，通过简单易懂的示例说明列表修改的核心概念和最佳实践。

---
+++ 01-Python-基础/64-Python-常用数据类型操作-列表-常用操作-修改/example.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-22 12:58:29
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 64-Python-常用数据类型操作-列表-常用操作-修改
+
+# 改：names[index] = new_value
+# 当我们要修改某个元素列表当中的某个元素时，一定是通过索引（下标）来修改的
+# names = ['张三', '李四', '王五']
+# print(names)
+# print(names[0])
+# names[0] = '小明'
+# print(names)
+# print(names[0])
+
+# names[3] = '小红' # IndexError: list assignment index out of range
