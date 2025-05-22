0ad707d5b0d8e7e1ac9855894d33eae4bdeb713b
Neo <431738916@qq.com>
2025-05-22 13:26:19 +0800

    docs: 添加 Python 列表删除操作的详细教程
    详细介绍了 Python 列表中删除元素的多种方法：
    - 使用 del 语句删除单个元素或整个列表
    - 使用 pop() 方法删除并返回指定位置的元素
    - 使用 remove() 方法删除第一个匹配的值
    
    包含了在循环中删除元素时的常见陷阱及其解决方案：
    - 从后向前遍历
    - 创建新列表
    - 使用 filter 函数
    
    添加了完整的示例代码和详细的操作说明，帮助读者更好地理解和掌握列表删除操作。

--- 01-Python-基础/63-Python-常用数据类型操作-列表-常用操作-删除/example.py
+++ 01-Python-基础/63-Python-常用数据类型操作-列表-常用操作-删除/example.py
@@ -7,3 +7,42 @@
 # @Email   : it666@linux.do
 # @Github  : https://github.com/BNTang
 # @Description: 63-Python-常用数据类型操作-列表-常用操作-删除
+
+# del 语句
+# nums = [1, 2, 3, 4, 5]
+# print(nums)
+
+# del nums[0]
+# print(nums)
+
+# nums = [1, 2, 3, 4, 5]
+
+# del nums
+
+# print(nums)
+
+# pop 方法
+# nums = [1, 2, 3, 4, 5]
+# print(nums)
+# print(nums.pop())
+# print(nums.pop(0))
+# print(nums)
+# print(nums.pop(11)) # IndexError: pop index out of range
+
+# remove 方法
+# nums = [1, 2, 3, 4, 5]
+# print(nums)
+# result = nums.remove(3)
+# print(result)
+# print(nums)
+
+# 注意：循环内删除元素带来的坑
+nums = [1, 2, 3, 4, 5, 5, 2, 3, 2]
+
+for i in nums:
+    # print(i)
+    if i == 2:
+        nums.remove(i) # 删除元素
+
+print(nums)
+# nums.remove(2)
