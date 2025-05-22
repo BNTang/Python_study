0edd3de92ce4a8f6d83caae159a93036c5b9d9c4
Neo <431738916@qq.com>
2025-05-22 13:11:31 +0800

    docs: 添加Python列表增加操作教程及示例代码
    新增Python列表增加操作的详细教程文档，包含以下内容：
    - append()方法添加单个元素
    - insert()方法在指定位置插入元素
    - extend()方法合并列表
    - 列表乘法运算扩展列表
    - 列表加法运算合并列表
    - 加法运算符与extend()方法的区别对比
    
    同时提供相应的示例代码，帮助读者更好地理解和掌握Python列表的各种增加操作方法。

---
+++ 01-Python-基础/62-Python-常用数据类型操作-列表-常用操作-增加/example.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-22 12:58:29
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 62-Python-常用数据类型操作-列表-常用操作-增加
+
+# 增，append
+# nums = [1, 2, 3, 4, 5]
+# print(nums)
+
+# nums.append(6)
+# print(nums.append(6))
+# print(nums)
+
+# 增，insert
+# nums = [1, 2, 3, 4, 5]
+# nums.insert(0, 5)
+# print(nums)
+
+# 增，extend：合并列表
+# nums = [1, 2, 3, 4, 5]
+# nums.extend([6, 7, 8, 9, 10])
+# print(nums)
+
+# 乘法运算：列表 * 整数
+# nums = [1, 2, 3, 4, 5]
+# nums = nums * 3
+# print(nums)
+
+# 加法运算：列表 + 列表
+# nums = [1, 2, 3, 4, 5]
+# nums2 = [6, 7, 8, 9, 10]
+# nums3 = nums + nums2
+# print(nums3)
+
+# 加法运算与extend的区别
+# nums = [1, 2, 3, 4, 5]
+# nums.extend([6, 7, 8, 9, 10])
+# print(nums)
+
+# nums = [1, 2, 3, 4, 5]
+# nums += [6, 7, 8, 9, 10]
+# print(nums)
+
+# n1 = [1, 2, 3, 4, 5]
+# n2 = [6, 7, 8, 9, 10]
+# print(n1 + n2)
---
+++ 01-Python-基础/63-Python-常用数据类型操作-列表-常用操作-删除/example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-22 12:58:29
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 63-Python-常用数据类型操作-列表-常用操作-删除
