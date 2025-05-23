cefbf0364f893d25cec46cf3cab1f111a91155f1
Neo <431738916@qq.com>
2025-05-23 21:38:07 +0800

    docs: 新增Python集合定义时的重要注意事项及示例

---
+++ 01-Python-基础/88-Python-常用数据类型操作-集合-常用操作-新增/example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-23
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 88-Python-常用数据类型操作-集合-常用操作-新增
