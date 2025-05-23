aa41782914a9b5670b5db219b4db05aa0a60cd94
Neo <431738916@qq.com>
2025-05-23 17:36:34 +0800

    docs: 新增Python集合操作的详细说明及示例代码

---
+++ 01-Python-基础/84-Python-常用数据类型操作-集合-概念/example.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-23
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 84-Python-常用数据类型操作-集合-概念
+
+# 集合
+
+# 无序的,不可随机访问的,不可重复的元家
+
+# 与数学中集合的概念类似，可对其进行交
+
+# 并、差、补等逻辑运算!
+
+# 概念
+
+# 分为可变集合和非可变集合
+
+# 增
+
+# 为可变集合
+
+# set
+
+# 删
+
+# 改
+
+# frozen
+
+# set
+
+# 不可变集合
+
+# 创建好之后,无法增删改
