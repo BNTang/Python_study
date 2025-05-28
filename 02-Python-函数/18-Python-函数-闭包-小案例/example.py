b1dfc3a1ae451430fe719f63fcf3848b4d293597
Neo <431738916@qq.com>
2025-05-28 23:31:17 +0800

    docs: 新增README.md和example.py文件，包含闭包小案例的基础信息

---
+++ 02-Python-函数/18-Python-函数-闭包-小案例/example.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-28
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 18-Python-函数-闭包-小案例
+
