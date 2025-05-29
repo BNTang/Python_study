7f57c09885782d53050587ee9bec3d50761802fc
Neo <431738916@qq.com>
2025-05-29 13:18:25 +0800

    docs: 新增多个README.md和example.py文件，包含Python函数装饰器的案例及其描述

---
+++ 02-Python-函数/23-Python-函数-装饰器-案例-4/example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-29
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 23-Python-函数-装饰器-案例-4
