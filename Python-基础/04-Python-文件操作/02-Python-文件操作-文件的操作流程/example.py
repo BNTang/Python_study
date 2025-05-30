7fe7415977c7fca7f5942ce0071225aa844810dc
Neo <431738916@qq.com>
2025-05-30 20:52:29 +0800

    docs: 新增README.md和example.py，介绍Python文件操作的基本概念及操作流程

---
+++ Python-基础/04-Python-文件操作/02-Python-文件操作-文件的操作流程/example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2025-5-30
+# @Author  : 程序员NEO
+# @FileName: example.py
+# @Software: VSCode
+# @Email   : it666@linux.do
+# @Github  : https://github.com/BNTang
+# @Description: 02-Python-文件操作-文件的操作流程
